28daf5e7dbdf140dbec91d62f1d2e318a32cf0c5
bamm99 <benjamin.mosso1701@alumnos.ubiobio.cl>
2023-11-17 00:16:51 -0300

    template base finish

--- allieci/db/migrate/20231117030538_devise_create_users.rb
+++ allieci/db/migrate/20231117030538_devise_create_users.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
-class DeviseCreateUsers < ActiveRecord::Migration[7.1]
+class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :rut
+      t.string :name
+      t.string :last_name
+      t.integer :role, default: 0  # 2: admin, 1: profesor, 0: alumno
+      t.integer :role_cursos, default: 0 # 0: no tiene permiso para crear cursos, 1: tiene permisos...
 
       ## Recoverable
       t.string   :reset_password_token
@@ -38,6 +43,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
+    add_index :users, :rut, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
