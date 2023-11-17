5a9ccb8545c1d5d12380533c569021873fdc9317
bamm99 <benjamin.mosso1701@alumnos.ubiobio.cl>
2023-11-17 04:42:17 -0300

    menu principal estudiante

--- allieci/app/javascript/terminal.js
+++ allieci/app/javascript/terminal.js
@@ -13,13 +13,9 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 
-    const prompt = () => {
-        terminal.write('\x1b[32mallieci:~$ \x1b[0m'); // Prefijo en verde
-    };
-
     terminal.open(document.getElementById('terminal-container'));
     terminal.focus(); 
-    prompt();
+    mostrarMenuPrincipal();
 
     let inputBuffer = '';
 
@@ -28,12 +24,10 @@ document.addEventListener("DOMContentLoaded", () => {
             terminal.write("\r\n");
             processCommand(inputBuffer);
             inputBuffer = ''; // Limpiar el buffer después de procesar el comando
-            prompt();
         } else if (domEvent.keyCode === 8) {
             // Manejar retroceso (borrar)
             if (inputBuffer.length > 0) {
                 inputBuffer = inputBuffer.substring(0, inputBuffer.length - 1);
-                // Borrar el último carácter en la terminal
                 terminal.write('\b \b');
             }
         } else {
@@ -43,10 +37,39 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     function processCommand(command) {
-        if (command.trim() === 'hola') {
-            terminal.writeln('Hola mundo');
-        } else {
-            terminal.writeln('Comando no reconocido: ' + command);
+        switch (command.trim()) {
+            case '1':
+                // Lógica para la opción 1
+                terminal.writeln('Opción 1 seleccionada');
+                break;
+            case '2':
+                // Lógica para la opción 2
+                terminal.writeln('Opción 2 seleccionada');
+                break;
+            case '3':
+                // Lógica para la opción 3
+                terminal.writeln('Opción 3 seleccionada');
+                break;
+            default:
+                terminal.writeln('Opción no válida');
+                mostrarMenuPrincipal();
+                break;
         }
+    }
+
+    function mostrarMenuPrincipal() {
+        terminal.clear();
+        terminal.writeln('----------------------------------------');
+        terminal.writeln('Bienvenido a ALL_IECI');
+        terminal.writeln('Software creado por Benjamin Mosso');
+        terminal.writeln('Estudiante de Ingenieria de Ejecucion en computacion e informatica');
+        terminal.writeln('Universidad Del Bio-Bio');
+        terminal.writeln('----------------------------------------');
+        terminal.writeln('               Menu');
+        terminal.writeln('1. Ver asignatura de un semestre');
+        terminal.writeln('2. Ver todas las asignaturas');
+        terminal.writeln('3. Ver curso de autoaprendizaje Linux');
+        terminal.writeln('----------------------------------------');
+        terminal.write('Ingrese una opcion: ');
     }
 });
